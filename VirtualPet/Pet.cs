de685e0c6a71615360a93f1b5d553258d3b6369b
NoahSekreter <nsekreter@gmail.com>
2017-04-09 13:43:43 -0400

    First commit

---
+++ VirtualPet/Pet.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+
+    class Pet
+    {
+        private string name;
+        private int[] stats = new int[7];
+        private Random r = new Random();
+
+        //Default constructor if the user didn't put in a name
+        public Pet()
+        {
+            this.name = "Cerea";
+
+            //stats contains all of a pet's stats : The minimum for a stat is 0 while the maximum is 100
+            stats[0] = 18; // 0 is love
+            stats[1] = 8; // 1 is smarts
+            stats[2] = 18; // 2 is hunger
+            stats[3] = 22; // 3 is thirst
+            stats[4] = 8; // 4 is boredom
+            stats[5] = 14; // 5 is sleepyness
+            stats[6] = 24; // 6 is sickness
+        }
+
+        public Pet(string name)
+        {
+            this.name = name;
+
+            //stats contains all of a pet's stats : The minimum for a stat is 0 while the maximum is 100
+            stats[0] = 18; // 0 is love
+            stats[1] = 8; // 1 is smarts
+            stats[2] = 18; // 2 is hunger
+            stats[3] = 22; // 3 is thirst
+            stats[4] = 8; // 4 is boredom
+            stats[5] = 14; // 5 is sleepyness
+            stats[6] = 24; // 6 is sickness
+        }
+
+        //Accessor methods, compressed to easily see the code and what it returns
+        public string Name { get { return name; } }
+        public int Love { get { return stats[0]; } }
+        public int Smarts { get { return stats[1]; } }
+        public int Hunger { get { return stats[2]; } }
+        public int Thirst { get { return stats[3]; } }
+        public int Boredom { get { return stats[4]; } }
+        public int Sleepyness { get { return stats[5]; } }
+        public int Sickness { get { return stats[6]; } }
+        
+        //All the actions are here
+        public void Feed()
+        {
+            //Reduce hunger
+            Console.WriteLine("You give " + name + " some algae, it seems really happy!");
+            stats[2] -= r.Next(25, 50);
+            Tick();
+        }
+        public void Hydrate()
+        {
+            //Reduce thirst
+            Console.WriteLine("You get " + name + " a small pool of water.\nIt sucks the water up playfully!");
+            stats[3] -= r.Next(25, 50);
+            Tick();
+        }
+        public void Play()
+        {
+            //Reduce boredom
+            Console.WriteLine("You play with your little slime pet, it's really appreciating your company!");
+            stats[4] -= r.Next(30, 55);
+            Tick();
+        }
+        public void Nap()
+        {
+            //Reduce sleepyness
+            Console.WriteLine("You bring " + name + " to its bed, letting it have sweet dreams.");
+            stats[5] -= r.Next(35, 50);
+            Tick();
+        }
+        public void Heal()
+        {
+            //Reduce sickness
+            Console.WriteLine("You go to the vet with " + name + ", and after a quick checkup your slime pet is feeling pretty great!");
+            stats[6] -= r.Next(45, 60);
+            Tick();
+        }
+        public void Train()
+        {
+            //Increase smarts
+            Console.WriteLine("You spend your time teaching " + name + " new tricks and words.");
+            stats[1] += r.Next(8, 14);
+            Tick();
+        }
+        public void Cuddle()
+        {
+            //Increase love and has a chance of doing something extra
+            Console.WriteLine("You relax with " + name + " cuddling beside you. It really enjoys your company!");
+            stats[0] += 6;
+            //Will do a random action, but there is a small chance nothing else will happen
+            int extra = r.Next(0, 3);
+            if(extra == 0)
+            {
+                Console.WriteLine(name + " fell asleep nuzzled up beside you. How cute!");
+                stats[5] -= r.Next(35, 50);
+            }
+            if (extra == 1)
+            {
+                Console.WriteLine("You decide to give your pet slime a few flakes of algae to snack on. " + name + " is now extra happy!");
+                stats[2] -= r.Next(20, 35);
+            }
+            if (extra == 2)
+            {
+                Console.WriteLine("You play a few video games. " + name + " seems quite interested!");
+                stats[4] -= r.Next(30, 45);
+            }
+            Tick();
+        }
+
+        public void SmartPet()
+        {
+            //Depending on how smart it is, your pet will have a number of chances to perform smart actions (Rounded down)
+            for(int i = 0; i < (int)Math.Floor((double)(stats[1] / 30)); i++)
+            {
+                //There is a one in three chance for each chance you get to perform a smart action
+                if(r.Next(0, 2) == 0)
+                {
+                    //Is hunger the biggest concern?
+                    if (stats[2] > stats[3] && stats[2] > stats[5])
+                    {
+                        Console.WriteLine(name + " decided to help itself to some extra algae flakes.");
+                        stats[2] -= r.Next(8,16);
+                    }
+                    //Is thirst the biggest concern?
+                    else if (stats[3] > stats[2] && stats[3] > stats[5])
+                    {
+                        Console.WriteLine(name + " went to hydrate itself a little bit.");
+                        stats[3] -= r.Next(8, 16);
+                    }
+                    //Is sleep the biggest concern?
+                    else
+                    {
+                        Console.WriteLine(name + " took a wee nap in its down time.");
+                        stats[5] -= r.Next(8, 16);
+                    }
+                }
+            }
+        }
+
+        //Makes changes every action the player takes. Can only be used by Pet class
+        private void Tick()
+        {
+            //Goes through all stats except love and smarts
+            for (int i = 2; i < stats.Length; i++)
+            {
+                if (stats[i] >= 85)
+                {
+                    //If any negative stat is too large, reduce love
+                    stats[0] -= r.Next(6,9);
+                }
+                else if (stats[i] < 50)
+                {
+                    stats[0] += r.Next(1,2);
+                }
+                //stats[i] += 3;
+            }
+            
+            //Gradually increase hungry, thirst, sleepyness, boredom, and sickness
+            stats[2] += r.Next(5, 10);
+            stats[3] += r.Next(5, 10);
+            stats[4] += r.Next(12, 16); //Boredom is a bit faster
+            stats[5] += r.Next(5, 10);
+            stats[6] += r.Next(4, 8); //Sickness is a bit slower
+
+            //Determines if your pet declares a smart action this turn
+            SmartPet();
+
+            for (int i = 0; i < stats.Length; i++)
+            {
+                if (stats[i] > 100)
+                {
+                    stats[i] = 100;
+                }
+                else if (stats[i] < 0)
+                {
+                    stats[i] = 0;
+                }
+            }
+        }
+
+
+        //Returns whatever condition your pet is in. i.e. hungry, thirsty, sleepy
+        public string Condition()
+        {
+            string condition = "";
+            int amount = 0;
+            if(stats[2] >= 85)
+            {
+                amount++;
+                condition += "hungry";
+            }
+            if (stats[3] >= 85)
+            {
+                amount++;
+                condition += "thirsty";
+            }
+            if (stats[5] >= 85)
+            {
+                amount++;
+                condition += "sleepy";
+            }
+            if (stats[6] >= 85)
+            {
+                amount++;
+                condition += "sickly";
+            }
+
+            //Make condition grammatically correct
+            if (amount >= 2)
+            {
+                condition = condition.Insert(condition.LastIndexOf('y', condition.Length-2) + 1, " and ");
+            }
+            if (amount == 3)
+            {
+                condition = condition.Insert(condition.IndexOf('y') + 1, ", ");
+            }
+            if (amount == 4)
+            {
+                condition = "hungry, thirsty, sleepy, and sickly";
+            }
+            return condition;
+        }
+        
+        //Pet stats
+        public void MyPetStatus()
+        {
+            //The different emotions of your pet
+            string[] image = new string[8];
+            if (stats[0] < 9 || stats[2] >= 85 || stats[3] >= 85 || stats[4] >= 85 || stats[5] >= 85 || stats[6] >= 85)
+            {
+                image[0] = "      _________      ";
+                image[1] = "    _/         \\_    ";
+                image[2] = "   /   __   __   \\   ";
+                image[3] = "  /               \\  ";
+                image[4] = " |     _______    |  ";
+                image[5] = " |    /       \\   |  ";
+                image[6] = "  \\              /   ";
+                image[7] = "   \\____________/    ";
+            }
+            else if(stats[0] >= 60)
+            {
+                image[0] = "      _________  <3  ";
+                image[1] = "    _/         \\_    ";
+                image[2] = "   /   ^     ^   \\   ";
+                image[3] = "  /               \\  ";
+                image[4] = " |    \\_______/   |  ";
+                image[5] = " |                |  ";
+                image[6] = "  \\              /   ";
+                image[7] = "   \\____________/    ";
+            }
+            else
+            {
+                image[0] = "      _________      ";
+                image[1] = "    _/         \\_    ";
+                image[2] = "   /             \\   ";
+                image[3] = "  /   O       O   \\  ";
+                image[4] = " |                |  ";
+                image[5] = " |        o       |  ";
+                image[6] = "  \\              /   ";
+                image[7] = "   \\____________/    ";
+            }
+
+            //Display image and stats
+            Console.WriteLine(image[0] + name);
+            Console.WriteLine(image[1] + "Health: " + (100 - stats[6]));
+            Console.WriteLine(image[2] + "Love: " + stats[0]);
+            Console.WriteLine(image[3] + "Smarts: " + stats[1]);
+            Console.WriteLine(image[4] + "Hunger: " + stats[2]);
+            Console.WriteLine(image[5] + "Thirst: " + stats[3]);
+            Console.WriteLine(image[6] + "Boredom: " + stats[4]);
+            Console.WriteLine(image[7] + "Sleepyness: " + stats[5]);
+        }
+    }
+}
---
+++ VirtualPet/Program.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pet myPet;
+            string response; //This will always be used for Console.ReadLine()
+            int selectedOption = 0;
+
+            Console.WriteLine("\n   < > < > < > Welcome welcome welcome to the magical pet shop! < > < > < >\n");
+            Console.WriteLine("I know you're dying to get your new pet, so let's get started!\nWe've saved a lovely little blue Slime just for you!");
+            Console.Write("What would you like to name your new pet? If you type in nothing, I'll give it a name for you! ");
+            response = Console.ReadLine();
+            if (response.ToLower() == "kek")
+            {
+                Kek(); //Easter egg. Infinite loop
+            }
+            if (response == "")
+            {
+                Console.WriteLine("Don't know a good name? I got one! \"Cerea\"!");
+                myPet = new Pet();
+            }
+            else
+            {
+                Console.WriteLine(response + "? I like it!");
+                myPet = new Pet(response);
+            }
+
+            Console.WriteLine("\nYou take your new slime pet home, it seems quite excited.");
+
+            do
+            {
+                //The options you have with your pet
+                Console.WriteLine();
+                Console.WriteLine("What would you like to do with " + myPet.Name + "?");
+                Console.WriteLine("1. Give food");
+                Console.WriteLine("2. Give water");
+                Console.WriteLine("3. Nap");
+                Console.WriteLine("4. Take to vet");
+                Console.Write("5. Play");
+                //If pet isn't feeling well, display the reason why
+                if (myPet.Hunger < 85 && myPet.Thirst < 85 && myPet.Sleepyness < 85 && myPet.Sickness < 85)
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine(" : " + myPet.Name + " is too " + myPet.Condition() + " to play.");
+                }
+                Console.Write("6. Train");
+                //If pet isn't feeling well, display the reason why
+                if (myPet.Hunger < 85 && myPet.Thirst < 85 && myPet.Sleepyness < 85 && myPet.Sickness < 85)
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine(" : " + myPet.Name + " is too " + myPet.Condition() + " to train.");
+                }
+                Console.Write("7. Cuddle");
+                //If love isn't sufficient, the player will see the message
+                if (myPet.Love >= 60)
+                {
+                    Console.WriteLine();
+                }
+                else if (myPet.Love < 60)
+                {
+                    Console.WriteLine(" : You require at least 60 Love to cuddle.");
+                }
+                Console.WriteLine("8. Check stats");
+                Console.WriteLine("9. Quit the game");
+
+                //Player enters in number
+                Console.Write("\n> ");
+                response = Console.ReadLine();
+                //Check to see if what they typed in was an actual number
+                while (!IsNumber(response))
+                {
+                    Console.WriteLine("\nSorry, could you type in a valid number?");
+                    Console.Write("\n> ");
+                    response = Console.ReadLine();
+                }
+                selectedOption = int.Parse(response);
+
+                Console.WriteLine();
+
+                //Do an action depending on what they entered
+                switch(selectedOption)
+                {
+                    case 1:
+                        Transition();
+                        myPet.Feed();
+                        break;
+                    case 2:
+                        Transition();
+                        myPet.Hydrate();
+                        break;
+                    case 3:
+                        Transition();
+                        myPet.Nap();
+                        break;
+                    case 4:
+                        Transition();
+                        myPet.Heal();
+                        break;
+                    case 5:
+                        //Won't work unless pet is healthy
+                        if (myPet.Hunger >= 85 || myPet.Thirst >= 85 || myPet.Sleepyness >= 85 || myPet.Sickness >= 85)
+                        {
+                            Console.WriteLine(myPet.Name + " isn't ready to play. Take care of your slime first!");
+                        }
+                        else
+                        {
+                            Transition();
+                            myPet.Play();
+                        }
+                        break;
+                    case 6:
+                        //Won't work unless pet is healthy
+                        if (myPet.Hunger >= 85 || myPet.Thirst >= 85 || myPet.Sleepyness >= 85 || myPet.Sickness >= 85)
+                        {
+                            Console.WriteLine(myPet.Name + " isn't ready to train. Take care of your slime first!");
+                        }
+                        else
+                        {
+                            Transition();
+                            myPet.Train();
+                        }
+                        break;
+                    case 7:
+                        if (myPet.Love < 60)
+                        {
+                            Console.WriteLine(myPet.Name + " isn't in the mood to cuddle. You gotta love your pet some more first!");
+                        }
+                        else
+                        {
+                            Transition();
+                            myPet.Cuddle();
+                        }
+                        break;
+                    case 8:
+                        myPet.MyPetStatus();
+                        break;
+                    case 9:
+                        break;
+                    default:
+                        Console.WriteLine("Sorry, that's not one of the available actions.");
+                        break;
+                }
+                //If the pet is not taken care of, end the game
+                if(myPet.Love == 0 || myPet.Sickness == 100 || myPet.Hunger == 100 || myPet.Thirst == 100 || myPet.Sleepyness == 100)
+                {
+                    Console.Write("\n...It seems you've neglected your pet a bit too much. ");
+                    selectedOption = 9;
+                }
+            } while (selectedOption != 9);
+
+            //If the player did not take care of their pet, display one of these messages
+            if (myPet.Love == 0)
+            {
+                Console.WriteLine(myPet.Name + " ran away.\n\n\t\t     < > < > < > GAME OVER < > < > < >\n");
+            }
+            else if (myPet.Sickness == 100)
+            {
+                Console.WriteLine(myPet.Name + " died from sickness.\n\n\t\t     < > < > < > GAME OVER < > < > < >\n");
+            }
+            else if (myPet.Hunger == 100)
+            {
+                Console.WriteLine(myPet.Name + " died from hunger.\n\n\t\t     < > < > < > GAME OVER < > < > < >\n");
+            }
+            else if (myPet.Thirst == 100)
+            {
+                Console.WriteLine(myPet.Name + " died from thirst.\n\n\t\t     < > < > < > GAME OVER < > < > < >\n");
+            }
+            else if (myPet.Sleepyness == 100)
+            {
+                Console.WriteLine(myPet.Name + " died from sleep deprivation.\n\n\t\t     < > < > < > GAME OVER < > < > < >\n");
+            }
+            //If they took care of their pet, display this instead
+            else
+            {
+                //Congradulate player for maxing out love
+                if(myPet.Love == 100)
+                {
+                    Console.WriteLine("Congradulations! " + myPet.Name + " loves you to bits!\nAs a reward, go restart the game, but put in \"kek\" as the name...\n");
+                }
+                Console.WriteLine("Thanks for playing!");
+            }
+        }
+
+        //Slow transition into things
+        static void Transition()
+        {
+            foreach (char c in ". . . ")
+            {
+                Console.Write(c);
+                System.Threading.Thread.Sleep(350);
+            }
+            Console.WriteLine("\n");
+        }
+
+        //Checks to see if the string is a number
+        static bool IsNumber(string str)
+        {
+            //Prevents fatal error if string is nothing or too large for the int to contain the number
+            if(str.Length == 0 || str.Length > 6)
+            {
+                return false;
+            }
+
+            //Checks every character to see if it is a number
+            foreach(char c in str)
+            {
+                if(c < '0' || c > '9')
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        //This is a silly and completely pointless feature, however it is vastly entertaining
+        static void Kek()
+        {
+            bool shrink = true;
+            string space1 = "             ";
+            string space2 = "";
+            string magic;
+            while (1 < 2) //deadmau5 reference. Also intentional infinite loop.
+            {
+                if (space1.Length == 13) //Should whitespace #1 shrink now?
+                {
+                    shrink = true;
+                }
+                if (space1.Length == 0) //Should whitespace #1 grow now?
+                {
+                    shrink = false;
+                }
+                if (shrink) //Shrink whitespace #1, grow whitespace #2
+                {
+                    space1 = space1.Substring(0, space1.Length - 1);
+                    space2 += " ";
+                }
+                else //Shrink whitespace #2, grow whitespace #1
+                {
+                    space2 = space2.Substring(0, space2.Length - 1);
+                    space1 += " ";
+                }
+
+                //Creating the magic!
+                magic = "KEK" + space1 + "KEK" + space2 + "KEK" + space2 + "KEK" + space1 + "KEK" + space1 + "KEK" + space2 + "KEK" + space2 + "KEK" + space1 + "KEK";
+
+                Console.WriteLine(magic); //Unleashing the magic!
+                System.Threading.Thread.Sleep(20); //Speed control. Bigger number = Slow, sensual ride. Smaller number = Maximum overkek!
+            }
+        }
+    }
+}
